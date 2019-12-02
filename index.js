13f17ce76a9a77deec8539bac3ccf38a548cf9b7
amomentforever0 <amomentforever0@gmail.com>
2019-12-02 21:22:21 +0100

    added pair to response

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var axios = require('axios');
 var app = express();
+const expressip = require('express-ip');
+
+app.use(expressip().getIpInfoMiddleware);
 
 var requestTime = function (req, res, next) {
   req.requestTime = Date.now();
@@ -16,11 +19,13 @@ app.use(requestTime);
 app.get('/', function (req, res) {
     console.log(req.query.pair);
     console.log(req.get('host'));
-  axios.get('https://api.kraken.com/0/public/Depth?pair=xbteur&count=4', config).then((response) => {
+    console.log(req.ipInfo);
+    const pair = req.query.pair || 'xbtusd';
+  axios.get(`https://api.kraken.com/0/public/Depth?pair=${pair}&count=4`, config).then((response) => {
     const pair = Object.keys(response.data.result)[0];
     const result = response.data.result;
     const { asks, bids } = result[pair];
-    const jsonResponse = Object.assign({}, { asks, bids });
+    const jsonResponse = Object.assign({}, { pair, asks, bids });
     res.json(jsonResponse);
   }).catch((err) => {
       res.send(err);
