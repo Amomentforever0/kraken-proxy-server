7f708adfaaadbca2111412fb2706476f8a8fce15
amomentforever0 <amomentforever0@gmail.com>
2019-12-02 21:27:09 +0100

    removed redundant ip

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var axios = require('axios');
 var app = express();
-const expressip = require('express-ip');
-
-app.use(expressip().getIpInfoMiddleware);
 
 var requestTime = function (req, res, next) {
   req.requestTime = Date.now();
@@ -18,7 +15,6 @@ app.use(requestTime);
 
 app.get('/', function (req, res) {
     console.log(req.query.pair);
-    console.log(req.get('host'));
     console.log(req.ipInfo);
     const pair = req.query.pair || 'xbtusd';
   axios.get(`https://api.kraken.com/0/public/Depth?pair=${pair}&count=4`, config).then((response) => {
